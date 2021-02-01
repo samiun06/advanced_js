00c41ebd210a073f1c90e5ece03031bbda707c35
Samiun Jahan <samiunjahan06@gmail.com>
2021-02-02 05:48:18 +0600

    second commit

---
+++ true-false.js
@@ -0,0 +1,27 @@
+/**Truthy
+ number > 0;
+ string !="";
+ "0";
+ true;
+ [];
+ {};
+
+ Falsy:
+ false;
+ 0;
+ "";
+ undefined;
+ null;
+ NaN;
+ */
+
+
+const name = "Nayeon";
+const condition = true;
+
+if(condition){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
