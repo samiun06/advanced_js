04c3b988e90004251e2114dc9a6cef6d8283b926
Samiun Jahan <samiunjahan06@gmail.com>
2021-02-02 06:28:01 +0600

    double-vs-tripple commit

---
+++ double-vs-tripple-=.js
@@ -0,0 +1,20 @@
+const first  = 2;
+const second = "2";
+
+// Double equal only checks the value tripple equal checks value and value type. 
+
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+} // output: condition is true.
+
+
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+} // output: condition is false.
+
